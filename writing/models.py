643d7f95b0cdb0e3fcaa0689691ab8737f333560
Nathan Huckabay <nathanhuckabay@gmail.com>
2025-05-15 19:54:10 -0500

    finished file structure and added model

--- writing/models.py
+++ writing/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=500)
+    body = models.TextField()
+    author = models.CharField(max_length=200)
+    public =models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
+    post_image = models.ImageField(upload_to='images/', null=True)
+
+    def __str__(self):
+        return self.title
